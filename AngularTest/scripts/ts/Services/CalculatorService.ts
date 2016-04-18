92c2a2d19f16e23490de0f92ee71f283da148d35
Martin de Boer <martin.de.boer@indivirtual.com>
2016-04-18 15:54:56 +0200

    added the httpget calls to the calculatorservice. TODO: use the repository pattern

--- AngularTest/scripts/ts/Services/CalculatorService.ts
+++ AngularTest/scripts/ts/Services/CalculatorService.ts
@@ -11,33 +11,95 @@
 
     class CalculatorService implements ICalculatorService {
         mainHub;
+        // TODO: use repository pattern.
+        divideUrl = "/divide";
+        multiplyUrl = "/multiply";
+        addUrl = "/add";
+        subtractUrl = "/subtract";
+        apiUrl = "/api/math"
 
-        static $inject: string[] = [];
+        static $inject: string[] = ["$q","$http"];
 
-        constructor(
-        ) {
+        constructor(private q: any, private http: ng.IHttpService)
+        {
 
 
         }
 
         Multiply(leftHand: number, rightHand: number): angular.IPromise<number>
         {
-            return null;
+            var defer = this.q.defer();
+                      
+            this.http.get(this.apiUrl + this.multiplyUrl)
+                .then((response: any) => {
+
+                    var result = response.data;
+
+                    defer.resolve(result);
+
+                }, (response: any) => {
+                    alert(response);
+                    var debug = 0;
+                });
+
+            return defer.promise;
         }
 
         Divide(leftHand: number, rightHand: number): angular.IPromise<number>
         {
-            return null;
+            var defer = this.q.defer();
+
+            this.http.get(this.apiUrl + this.divideUrl)
+                .then((response: any) => {
+
+                    var result = response.data;
+
+                    defer.resolve(result);
+
+                }, (response: any) => {
+                    alert(response);
+                    var debug = 0;
+                });
+
+            return defer.promise;
         }
 
         Add(leftHand: number, rightHand: number): angular.IPromise<number>
         {
-            return null;
+            var defer = this.q.defer();
+
+            this.http.get(this.apiUrl + this.addUrl)
+                .then((response: any) => {
+
+                    var result = response.data;
+
+                    defer.resolve(result);
+
+                }, (response: any) => {
+                    alert(response);
+                    var debug = 0;
+                });
+
+            return defer.promise;
         }
 
         Subtract(leftHand: number, rightHand: number): angular.IPromise<number>
         {
-            return null;
+            var defer = this.q.defer();
+
+            this.http.get(this.apiUrl + this.subtractUrl)
+                .then((response: any) => {
+
+                    var result = response.data;
+
+                    defer.resolve(result);
+
+                }, (response: any) => {
+                    alert(response);
+                    var debug = 0;
+                });
+
+            return defer.promise;
         }
 
       
